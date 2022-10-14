1632bf3c8dd086119bc215aa41f5afd36239e72a
Dmitrii Volkov <d.volkov@iqdev.digital>
2022-10-14 18:08:26 +0500

    wip

--- next.config.js
+++ next.config.js
@@ -1,7 +1,45 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    i18n: {
+        locales: ['main', 'tmn', 'msk'],
+        defaultLocale: 'main',
+
+        domains: [
+            {
+                domain: 'localhost',
+                defaultLocale: 'main',
+                http: true,
+            },
+            {
+                domain: 'tmn.localhost',
+                defaultLocale: 'tmn',
+                http: true,
+            },
+            {
+                domain: 'msk.localhost',
+                defaultLocale: 'msk',
+                http: true,
+            },
+        ],
+    },
+    // async redirects() {
+    //     return [
+    //         {
+    //             source: '/:path*)',
+    //             has: [
+    //                 {
+    //                     type: 'host',
+    //                     value: 'spb.localhost:3000',
+    //                 },
+    //             ],
+    //             permanent: true,
+    //             destination: 'google.ru',
+    //         },
+    //     ]
+    // },
+};
+
+module.exports = nextConfig;
